468b4e355735008eb8f6364b9fbe43006196241d
ZeeshanNiyoin <zeeshan.khan@niyoin.com>
2024-04-02 16:20:37 +0530

    Update 2024_04_01_140046_create_tickets.php

--- database/migrations/2024_04_01_140046_create_tickets.php
+++ database/migrations/2024_04_01_140046_create_tickets.php
@@ -16,6 +16,7 @@ class CreateTickets extends Migration
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
+            $table->string('complaint_id');
             $table->string('subject')->nullable();
             $table->string('email')->nullable();
             $table->string('product_type')->nullable();
