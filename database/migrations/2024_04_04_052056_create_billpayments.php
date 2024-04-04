82118948739c2ef06d0a8522b63dc66f87744824
Rahul Singh <94079091+RahulSRajawat@users.noreply.github.com>
2024-04-04 10:57:20 +0530

    grr
    gr

--- database/migrations/2024_04_04_052056_create_billpayments.php
+++ database/migrations/2024_04_04_052056_create_billpayments.php
@@ -15,6 +15,23 @@ class CreateBillpayments extends Migration
     {
         Schema::create('billpayments', function (Blueprint $table) {
             $table->id();
+            $table->string('operator');
+            $table->integer('canumber');
+            $table->string('amount');
+            $table->string('referenceid');
+            $table->integer('latitude');
+            $table->string('longitude');
+            $table->integer('mode');
+            $table->string('bill_fetch');
+            $table->integer('status');
+            $table->string('operatorid');
+            $table->integer('ackno');
+            $table->integer('message');
+            $table->integer('merchant_code');
+            $table->string('uuid');
+            $table->integer('orderid');
+            $table->integer('cummison');
+            $table->string('ballance');
             $table->timestamps();
         });
     }
