292cd90be3c6efd67ba4ca7bbc706d49e45f5880
Rukhsar-ansari-niyoin <157568936+Rukhsar-ansari-niyoin@users.noreply.github.com>
2024-04-04 11:10:09 +0530

    fdf

--- database/migrations/2024_04_04_053939_create_dmt.php
+++ database/migrations/2024_04_04_053939_create_dmt.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCms extends Migration
+class CreateDmt extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCms extends Migration
      */
     public function up()
     {
-        Schema::create('_cms', function (Blueprint $table) {
+        Schema::create('dmt', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateCms extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_cms');
+        Schema::dropIfExists('dmt');
     }
 }
